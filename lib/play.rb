c23f0655f3a58dbe1130c25b3207b6364da62208
Maikaru <maikaru@maikaru.com>
2021-06-02 02:20:10 +0000

    first commit

--- lib/play.rb
+++ lib/play.rb
@@ -18,7 +18,9 @@ end
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
-
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
@@ -26,7 +28,8 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  index = input_to_index(input)
+  index = input_to_index(user_input)
+  token = current_player
   if valid_move?(board, index)
     move(board, index)
     display_board(board)
@@ -36,3 +39,75 @@ def turn(board)
 end
 
 # Define your play method below
+def play(board)
+  counter = 0
+  until counter == 9
+  turn(board)
+  counter += 1
+ end
+end
+def play(board)
+  turn(board) until over?(board)
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
+def turn_count(board)
+  board.count{|token| token == "X" || token == "O"}
+end
+def current_player(board)
+  #if the turn count is an even number, that means O just went, so the next/current player is X
+  num_turns = turn_count(board)
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end
+def full?(board)
+   board.all? {|i| i == "X" || i == "O"}
+end
+def winner(board)
+   if won?(board)
+      return board [won?(board)[0]]
+   end
+end
+def over?(board)
+   if draw?(board) || won?(board) || full?(board)
+     return true
+   end
+end
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif !won?(board) && !full?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
+
+def won?(board)
+ WIN_COMBINATIONS.each do |win_combination|
+   win_index_1 = win_combination[0]
+   win_index_2 = win_combination[1]
+   win_index_3 = win_combination[2]
+   position_1 = board[win_index_1] # value of board at win_index_1
+   position_2 = board[win_index_2] # value of board at win_index_2
+   position_3 = board[win_index_3] # value of board at win_index_3
+   position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+ end
+end
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
